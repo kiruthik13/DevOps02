5115d1f7b5c55c2df8252fa5d713cc7a23fb4a2a
kiruthik13 <125745097+kiruthik13@users.noreply.github.com>
2025-02-05 13:35:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
     echo hi123
-    docker build -t test .
+   sh 'chmod +x build.sh'
+   sh './build.sh'
     docker login -u kiruthik1304 -p kiruthik@13
     docker tag test kiruthik1304/devopstask2
     docker push kiruthik1304/devopstask2
